cecdcca5ec39f5e0d98abf2d5694b3405b8a97a8
Emerson Warhman <edwarhman@gmail.com>
2024-02-03 23:20:28 -0400

    move tableDraggable logic to external hook

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,34 +10,31 @@ import { Card } from "./Card"
 import { IColumn, IColumnItem } from "@/services/boards"
 import { CreationCard } from "./CreationCard"
 import { MouseSensor, TouchSensor } from "@/services/fixedSensors"
+import useBoardDraggeable from "@/hooks/useBoardDraggeable"
 
 interface Props {
     items: IColumn[]
 }
 
-interface ItemId {
-    column: string
-    item: IColumnItem
-}
-
 export default function Table({ items }: Props) {
-    const [columns, setColumns] = useState<IColumn[] | null>(items)
-    const [activeItem, setActiveItem] = useState<ItemId | null>()
-    const [activeColumn, setActiveColumn] = useState<IColumn | null>()
-
     const sensors = useSensors(
         useSensor(MouseSensor),
         useSensor(TouchSensor)
     );
-
-    useEffect(() => {
-        console.log('al cambiar columnas', { columns })
-
-    }, [columns])
+    const {
+        columns,
+        activeColumn,
+        activeItem,
+        handleDragStart,
+        handleDragOver,
+        handleDragEnd,
+        createNewColumn,
+        createNewItem
+    } = useBoardDraggeable(items)
 
     return (
         <DndContext
-            onDragStart={handleItemDragStart}
+            onDragStart={handleDragStart}
             onDragOver={handleDragOver}
             onDragEnd={handleDragEnd}
             sensors={sensors}
@@ -70,226 +67,6 @@ export default function Table({ items }: Props) {
         </DndContext >
     )
 
-    function findColumnById(id: string): IColumn | undefined {
-        return columns?.find(col => col.id === id)
-    }
-
-    function findColumnByItemId(itemId: string): IColumn | undefined {
-        return columns?.find((col) => col.items.find(item => item.id === itemId))
-    }
-
-    function findItem(id: string): ItemId | undefined {
-        const column = findColumnByItemId(id)
-        console.log({ column })
-        if (!column)
-            return
-        const item = column?.items.find((item) => item.id === id)
-
-        if (!item)
-            return
-
-        return {
-            column: column?.id,
-            item: item
-        }
-    }
-
-    function handleItemDragStart(event: any) {
-        const { active } = event
-        const activeColumn = findColumnById(active.id)
-        setActiveColumn(activeColumn)
-        const activeItem = findItem(active.id)
-        setActiveItem(activeItem)
-    }
-
-    function handleDragOver(event: any) {
-        const { active, over, draggingRect } = event
-        const { id } = active
-        if (!over) {
-            return
-        }
-        const { id: overId } = over
-
-        const activeColumn = findColumnByItemId(active.id)
-        const overColumn = findColumnByItemId(over.id)
-        const activeItem = findItem(id)
-
-        if (
-            !activeItem ||
-            !activeColumn ||
-            !overColumn ||
-            activeColumn.id === overColumn.id
-        ) {
-            return
-        }
-
-        console.log({ over })
-
-        console.log('its different')
-        let newItemIdx = 0
-
-        if (overColumn) {
-            const overItem = findItem(overId)
-            if (!overItem) {
-                return
-            }
-            newItemIdx = overColumn.items.indexOf(overItem.item) + 1
-
-        } else {
-            newItemIdx = 0
-        }
-
-        console.log({ newItemIdx })
-
-        setColumns((prevColumns) => {
-            if (!prevColumns) {
-                return prevColumns
-            }
-            console.log({ prevColumns })
-            const newTable = prevColumns.map((prevCol, prevColIdx) => {
-                if (prevCol.id === overColumn.id) {
-                    let newOverColumnItems
-
-                    if (newItemIdx !== 0) {
-                        newOverColumnItems = [
-                            ...prevCol.items.slice(0, newItemIdx),
-                            activeItem?.item,
-                            ...prevCol.items.slice(newItemIdx)
-
-                        ]
-                    } else {
-                        newOverColumnItems = [
-                            activeItem.item,
-                            ...prevCol.items
-                        ]
-                    }
-
-                    console.log({ newOverColumnItems })
-
-                    return {
-                        ...prevCol,
-                        items: newOverColumnItems
-                    }
-
-                }
-                if (prevCol.id === activeColumn.id) {
-                    const newActiveColumnItems = prevCol.items.filter(item => item.id !== id)
-                    return {
-                        ...prevCol,
-                        items: newActiveColumnItems
-                    }
-                }
-
-                return prevCol
-            })
-            console.log({ newTable })
-            return newTable
-        })
-
-    }
-
-    function handleDragEnd(event: any) {
-        const { active, over } = event
-
-        const column = findColumnById(active.id)
-
-        if (column) {
-            if (active.id !== over.id) {
-
-                setColumns((prevCols: any) => {
-                    const oldIndex = prevCols.findIndex((el: any) => (
-                        el.id === active.id
-                    ))
-                    const newIndex = prevCols.findIndex((el: any) => (
-                        el.id === over.id
-                    ))
-
-                    return arrayMove(prevCols, oldIndex, newIndex)
-                })
-            }
-        }
-
-        const item = findItem(active.id)
-
-        if (item) {
-            if (active.id !== over.id) {
-                setColumns((prevCols: IColumn[] | null) => {
-
-                    if (!prevCols) {
-                        return prevCols
-                    }
-
-                    const newColumns = prevCols.map(prevCol => {
-                        if (prevCol.id === item.column) {
-                            const oldIndex = prevCol.items.findIndex((item: IColumnItem) => (item.id === active.id))
-                            const newIndex = prevCol.items.findIndex((item: IColumnItem) => (item.id === over.id))
-
-                            return {
-                                ...prevCol,
-                                items: arrayMove(prevCol.items, oldIndex, newIndex)
-                            }
-                        }
-
-                        return prevCol
-                    })
-
-                    return newColumns
-                })
-            }
-        }
-
-        setActiveItem(null)
-        setActiveColumn(null)
-    }
-
-    function createNewColumn(title: string) {
-        const newColumn: IColumn = {
-            id: new Date().toString(),
-            title,
-            items: []
-        }
-
-        setColumns(prev => {
-            if (!prev) {
-                return prev
-            }
-
-            return [
-                ...prev,
-                newColumn
-            ]
-        })
-    }
-
-    function createNewItem(columnId: string, title: string) {
-        const newItem: IColumnItem = {
-            id: new Date().toString(),
-            title,
-            description: ''
-        }
-
-        console.log({ newItem })
-
-        setColumns(prev => {
-            if (!prev) {
-                return prev
-            }
-            const newColumns = prev?.map(col => {
-                if (col.id === columnId) {
-                    const newList = [...col.items, newItem]
-                    return {
-                        ...col,
-                        items: newList
-                    }
-                }
-                return col
-            })
-
-            console.log({ newColumns })
-
-            return newColumns
-        })
-    }
 
     function handleNewColumn(params: any) {
         createNewColumn(params.title)
---
+++ src/hooks/useBoardDraggeable.ts
@@ -0,0 +1,249 @@
+import { IColumn, IColumnItem } from "@/services/boards";
+import { arrayMove } from "@dnd-kit/sortable";
+import { useState } from "react";
+
+
+interface ItemId {
+    column: string
+    item: IColumnItem
+}
+
+export default function useBoardDraggeable(lists: IColumn[]) {
+    const [columns, setColumns] = useState<IColumn[] | null>(lists)
+    const [activeItem, setActiveItem] = useState<ItemId | null>()
+    const [activeColumn, setActiveColumn] = useState<IColumn | null>()
+
+    function findColumnById(id: string): IColumn | undefined {
+        return columns?.find(col => col.id === id)
+    }
+
+    function findColumnByItemId(itemId: string): IColumn | undefined {
+        return columns?.find((col) => col.items.find(item => item.id === itemId))
+    }
+
+    function findItemById(id: string): ItemId | undefined {
+        const column = findColumnByItemId(id)
+        console.log({ column })
+        if (!column)
+            return
+        const item = column?.items.find((item) => item.id === id)
+
+        if (!item)
+            return
+
+        return {
+            column: column?.id,
+            item: item
+        }
+    }
+
+    function handleDragStart(event: any) {
+        const { active } = event
+        const activeColumn = findColumnById(active.id)
+        setActiveColumn(activeColumn)
+        const activeItem = findItemById(active.id)
+        setActiveItem(activeItem)
+    }
+
+    function handleDragOver(event: any) {
+        const { active, over, draggingRect } = event
+        const { id } = active
+        if (!over) {
+            return
+        }
+        const { id: overId } = over
+
+        const activeColumn = findColumnByItemId(active.id)
+        const overColumn = findColumnByItemId(over.id)
+        const activeItem = findItemById(id)
+
+        if (
+            !activeItem ||
+            !activeColumn ||
+            !overColumn ||
+            activeColumn.id === overColumn.id
+        ) {
+            return
+        }
+
+        console.log({ over })
+
+        console.log('its different')
+        let newItemIdx = 0
+
+        if (overColumn) {
+            const overItem = findItemById(overId)
+            if (!overItem) {
+                return
+            }
+            newItemIdx = overColumn.items.indexOf(overItem.item) + 1
+
+        } else {
+            newItemIdx = 0
+        }
+
+        console.log({ newItemIdx })
+
+        setColumns((prevColumns) => {
+            if (!prevColumns) {
+                return prevColumns
+            }
+            console.log({ prevColumns })
+            const newTable = prevColumns.map((prevCol, prevColIdx) => {
+                if (prevCol.id === overColumn.id) {
+                    let newOverColumnItems
+
+                    if (newItemIdx !== 0) {
+                        newOverColumnItems = [
+                            ...prevCol.items.slice(0, newItemIdx),
+                            activeItem?.item,
+                            ...prevCol.items.slice(newItemIdx)
+
+                        ]
+                    } else {
+                        newOverColumnItems = [
+                            activeItem.item,
+                            ...prevCol.items
+                        ]
+                    }
+
+                    console.log({ newOverColumnItems })
+
+                    return {
+                        ...prevCol,
+                        items: newOverColumnItems
+                    }
+
+                }
+                if (prevCol.id === activeColumn.id) {
+                    const newActiveColumnItems = prevCol.items.filter(item => item.id !== id)
+                    return {
+                        ...prevCol,
+                        items: newActiveColumnItems
+                    }
+                }
+
+                return prevCol
+            })
+            console.log({ newTable })
+            return newTable
+        })
+
+    }
+
+    function handleDragEnd(event: any) {
+        const { active, over } = event
+
+        const column = findColumnById(active.id)
+
+        if (column) {
+            if (active.id !== over.id) {
+
+                setColumns((prevCols: any) => {
+                    const oldIndex = prevCols.findIndex((el: any) => (
+                        el.id === active.id
+                    ))
+                    const newIndex = prevCols.findIndex((el: any) => (
+                        el.id === over.id
+                    ))
+
+                    return arrayMove(prevCols, oldIndex, newIndex)
+                })
+            }
+        }
+
+        const item = findItemById(active.id)
+
+        if (item) {
+            if (active.id !== over.id) {
+                setColumns((prevCols: IColumn[] | null) => {
+
+                    if (!prevCols) {
+                        return prevCols
+                    }
+
+                    const newColumns = prevCols.map(prevCol => {
+                        if (prevCol.id === item.column) {
+                            const oldIndex = prevCol.items.findIndex((item: IColumnItem) => (item.id === active.id))
+                            const newIndex = prevCol.items.findIndex((item: IColumnItem) => (item.id === over.id))
+
+                            return {
+                                ...prevCol,
+                                items: arrayMove(prevCol.items, oldIndex, newIndex)
+                            }
+                        }
+
+                        return prevCol
+                    })
+
+                    return newColumns
+                })
+            }
+        }
+
+        setActiveItem(null)
+        setActiveColumn(null)
+    }
+
+    function createNewColumn(title: string) {
+        const newColumn: IColumn = {
+            id: new Date().toString(),
+            title,
+            items: []
+        }
+
+        setColumns(prev => {
+            if (!prev) {
+                return prev
+            }
+
+            return [
+                ...prev,
+                newColumn
+            ]
+        })
+    }
+
+    function createNewItem(columnId: string, title: string) {
+        const newItem: IColumnItem = {
+            id: new Date().toString(),
+            title,
+            description: ''
+        }
+
+        console.log({ newItem })
+
+        setColumns(prev => {
+            if (!prev) {
+                return prev
+            }
+            const newColumns = prev?.map(col => {
+                if (col.id === columnId) {
+                    const newList = [...col.items, newItem]
+                    return {
+                        ...col,
+                        items: newList
+                    }
+                }
+                return col
+            })
+
+            console.log({ newColumns })
+
+            return newColumns
+        })
+    }
+    return {
+        columns,
+        activeColumn,
+        activeItem,
+        createNewColumn,
+        createNewItem,
+        findColumnById,
+        findColumnByItemId,
+        findItemById,
+        handleDragStart,
+        handleDragOver,
+        handleDragEnd,
+    }
+}
